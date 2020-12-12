9e83796d4272aff8c306b2e27090552fb02142f8
fintank <finapptank@gmail.com>
2020-12-12 00:24:20 -0500

    added post and like model

---
+++ models/Post.js
@@ -0,0 +1,70 @@
+const {Model, DataType, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+    static uplike(body, models) {
+        return model.Like.create({
+            user_id: body.user_id,
+            post_id: body.post_id,
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'title',
+                    'body',
+                    'created_at',
+                    [
+                        sequlelize.literal('(SELECT COUNT(*) FROM like where post.id = like.post_id)'),
+                        'like_count'
+                    ]
+                ]
+            });
+        });
+    }
+}
+
+Post.init(
+    {
+        id: {
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey:true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull:true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id'
+            }
+        },
+        image_id: {
+            type:DataTypes.INTEGER,
+            allowNull:true,
+            references: {
+                model:'image',
+                key:'id'
+            }
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        timestamps: true,
+        underscored: true,
+        modelName: 'post'
+      }
+);
+
+module.exports = Post;
